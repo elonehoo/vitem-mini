19d357b94a23eb8af78f4ed4a7324f16ab58698d
xiaoxunyao <huchengyea@163.com>
2022-04-28 11:19:49 +0800

    fix: add `dark` mode

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Index from '~/pages/index.vue'
+import Footer from '~/components/Footer.vue'
 </script>
 
 <template>
   <main class="font-sans px-4 py-10 text-center gray-700 dark:gray-200">
     <Index />
+    <Footer />
   </main>
 </template>
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { reactive } from '@vue/reactivity';
+
+const isDark = reactive({
+  value: false,
+})
+
+function toggleDark() {
+  isDark.value = document.documentElement.classList.toggle('dark')
+  console.log(isDark.value)
+}
+</script>
+
+<template>
+  <nav class="text-xl mt-6 inline-flex gap-2">
+    <button @click="toggleDark()" class="text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out !outline-none hover:opacity-100 hover:text-teal-600">
+      <span v-if="isDark.value" class="iconify" data-icon="carbon:moon" data-inline="false"></span>
+      <span v-else class="iconify" data-icon="carbon:sun" data-inline="false"></span>
+    </button>
+    <a href="https://github.com/elonehoo/vitem-mini" target="_blank" class="text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out !outline-none hover:opacity-100 hover:text-teal-600">
+      <span class="iconify" data-icon="carbon:logo-github" data-inline="false"></span>
+    </a>
+  </nav>
+</template>
