08c4375b380c3630b920fcffcec2aea11f726a4f
xiaoxunyao <huchengyea@163.com>
2022-04-28 10:45:23 +0800

    fix: update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '~/App.vue'
+import '~/style/index.css'
+import '@purge-icons/generated'
 
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import PurgeIcons from 'vite-plugin-purge-icons'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +11,10 @@ export default defineConfig({
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-  plugins: [vue()]
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+    PurgeIcons()
+  ]
 })
